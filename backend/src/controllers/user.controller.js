c2be8c60fbb4ed199d2a2ec4a0b98772f85517a3
Hagilaash <hagilaash@gmail.com>
2026-01-13 23:53:46 +0530

    creating a new user and adding validation for existing user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,25 +4,35 @@ const registerUser = async (req, res) => {
     try {
         const { username, password, email } = req.body;
 
-        // basic validation
+        // BASIC VALIDATION
         if (!username || !password || !email) {
-            return res.status(400).json({ message: 'All fields are required' });
+            if (!username || !password || !email) {
+                    return res.status(400).json({
+                        message: "ALL FIELDS ARE REQUIRED",
+                        data: req.body
+                    });
+            }
         }
 
-        // // check if user already exists
-        // const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-        // if (existingUser) {
-        //     return res.status(409).json({ message: 'Username or email already in use' });
-        // }
+        // CHECK IF USER ALREADY EXISTS
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
 
-        // create new user
+        if (existingUser) {
+            return res.status(409).json({
+                message: 'USER ALREADY EXISTS',
+                data: req.body});
+        }
+
+        // CREATE NEW USER
         const user = await User.create({ 
             username, 
             password, 
             email: email.toLowerCase(),
             loggedIn: false 
         });
-        res.status(201).json({ message: 'User registered successfully', user:{ id:user.id, username:user.username, email:user.email } });
+
+        res.status(201).json({ message: 'User registered successfully', 
+            user:{ id:user.id, username:user.username, email:user.email } });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
